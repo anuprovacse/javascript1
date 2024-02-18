2244f505113b2ae7e297e6f6189838daa29f2d2d
anuprovacse <anuprova77@gmail.com>
2024-02-18 22:23:17 +0100

    js

---
+++ script.js
@@ -0,0 +1,18 @@
+window.alert("I am learning javascript");
+document.write("I am Anuprova Mondal ");
+
+document.getElementById("idh1").innerHTML = "This is H1";
+document.getElementById("idh3").innerHTML = "This is H3";
+
+console.log("This is JavaScript");
+
+var x;
+x = prompt("Enter Your Name: ");
+document.write(x);
+
+var C = prompt("Please enter temperature: ");
+var result = 9/5*C + 32;
+alert("Fahrenheit: " + result + " Degree");
+console.log("Task Complete!");
+
+ 
